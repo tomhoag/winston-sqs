5034c227b98732005e9afd84b8fd0453dfa9ca93
Jakub Holy <jakub.holy@iterate.no>
2015-03-13 11:40:09 +0100

    Support passing in aws_credentials
    Pass in AWS.Credentials instance instead of 
    id+secret.
    
    Beware: The EvnironmentCredentials fail for me
    with “no such method get” in its constructor.

--- README.md
+++ README.md
@@ -16,15 +16,20 @@ The Winston SQS transport depends on [aws-sdk](https://github.com/aws/aws-sdk-js
 
 The constructor options are:
 
-* __aws_accesskeyid:__ Your Amazon Public Key. *[required]*
-* __aws_secretaccesskey:__ Your Amazon Secret. *[required]*
+* __aws_credentials:__ [`AWS.Credentials`][1] instance, f.ex. `AWS.EC2MetadataCredentials` 
+  or `AWS.EnvironmentCredentials` *[required this or key+secret]*
+* __aws_accesskeyid:__ Your Amazon Public Key. *[required this or aws_credentials]*
+* __aws_secretaccesskey:__ Your Amazon Secret. *[required this or aws_credentials]*
 * __aws_queueurl:__ The specific queue URL to write to.  Found in your AWS Console. *[required]*
 * __aws_region:__ AWS Region to use. (default: `us-east-1`)
 * __level:__ default logging level. (default: `info`)
 * __aws_sslenabled:__ whether to use SSL in communication with SQS. (default: true)
 * __aws_maxretries:__ number of times to retry communication with SQS. (default: 3)
+* __debug:__ log error/response after each message sent (default: false)
+* __formatter:__ custom log formatting function, as in the core Winston transports (default: undefined)
 * __timestamp:__ whether to include a timestamp in the queue message. (default: true)
 
 Note that metadata will be automatically 'stringified.'
 
 [0]: https://github.com/flatiron/winston
+[1]: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Credentials.html
--- test/winston-sqs-test.js
+++ test/winston-sqs-test.js
@@ -1,14 +1,9 @@
+var aws = require("aws-sdk");
 var vows = require('vows');
 var assert = require('assert');
 var winston = require('winston');
 var helpers = require('winston/test/helpers');
 var SQS = require('../lib/winston-sqs').SQS;
-
-var transport = new (SQS)({
-    aws_queueurl: "queueurl",
-    aws_accesskeyid: "publickey",
-    aws_secretaccesskey: "secretkey"
-});
 
 function assertSQS (transport) {
     assert.instanceOf(transport, SQS);
@@ -33,17 +28,52 @@ vows.describe('winston-sqs').addBatch({
         },
         "(accessKeyId and) secretAccessKey (or credentials)": function (){
             assert.throws(function() {new (SQS)(without(allOptions, "aws_secretaccesskey"));}, Error);
+        },
+        "aws_credentials must be aws.Credentials": function (){
+            assert.throws(function() {new (SQS)({
+                aws_queueurl: "queueurl",
+                aws_credentials: "not an aws.Credentials instnace"
+            });}, Error);
         }
 
     },
+    "The creation of an Amazon SQS Transport instance": {
+        "should succeed with aws_accesskeyid and aws_secretaccesskey": function() {
+            assertSQS(new (SQS)({
+                aws_queueurl: "queueurl",
+                aws_accesskeyid: "publickey",
+                aws_secretaccesskey: "secretkey"
+            }));
+        },
+        "should succeed with aws_credentials": function() {
+            assertSQS(new (SQS)({
+                aws_queueurl: "queueurl",
+                aws_credentials: new aws.Credentials("dummy id", "dummy secret")
+            }));
+        },
+        "should succeed with aws_credentials using a subclass": function() {
+            assertSQS(new (SQS)({
+                aws_queueurl: "queueurl",
+                aws_credentials: new aws.EnvironmentCredentials()
+            }));
+        }
+    },
     "An instance of the Amazon SQS Transport": {
         "should have the proper methods defined": function () {
-            assertSQS(transport);
+            assertSQS(new (SQS)(allOptions));
         }
-        /* Uncomment this test when correct AWS credentials and queue URL are applied above.
-        ,"the log() method": helpers.testNpmLevels(transport, "should log messages to Amazon SQS", function (ign, err, logged) {
-            assert.isTrue(!err);
-            assert.isTrue(logged);
-        })*/
+         /* // Uncomment this test when correct AWS credentials and queue URL are provided in env vars
+        ,"the log() method": helpers.testNpmLevels(
+            new (SQS)({
+                aws_queueurl: process.env.AWS_QUEUEURL,
+                aws_accesskeyid: process.env.AWS_ACCESS_KEY_ID,
+                aws_secretaccesskey: process.env.AWS_SECRET_ACCESS_KEY
+            }),
+            "should log messages to Amazon SQS",
+            function (ign, err, logged) {
+                console.log("Assertin results....");
+                assert.isTrue(!err);
+                assert.isTrue(logged);
+        })// */
     }
 }).export(module);
